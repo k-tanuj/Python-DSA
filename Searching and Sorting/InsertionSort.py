c20d6fd956b3c376f5f02c19697bbcea6bf0b7b2
k-tanuj <tanujkumawat3008@gmail.com>
2026-07-13 21:44:01 +0530

    Insertion sort

--- Searching and Sorting/InsertionSort.py
+++ Searching and Sorting/InsertionSort.py
@@ -1,21 +1,20 @@
 def InsertionSort(arr):
-    n= len(arr)
+    n = len(arr)
 
-    for current in range(1,n):
-        current = arr[current]
+    for current in range(1, n):
+        currentElement = arr[current]
         correctPosition = current - 1
 
-        while correctPosition >= 0:
-            if arr[correctPosition]>current:
-                break
-            else:
-                arr[correctPosition + 1] = arr[correctPosition]
-                correctPosition -= 1
-        
-        
+        while correctPosition >= 0 and arr[correctPosition] > currentElement:
+            arr[correctPosition + 1] = arr[correctPosition]
+            correctPosition -= 1
 
-    return arr 
+        arr[correctPosition + 1] = currentElement
 
-unsorted_list = [12,25,11,34,90,22]
+    return arr
+
+
+unsorted_list = [12, 25, 11, 34, 90, 22]
 sorted_list = InsertionSort(unsorted_list)
+
 print("Sorted elements:", sorted_list)
